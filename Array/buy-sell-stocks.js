5fc2bfbaf16ede0cf6a0e1547ecc04ab491b5c73
abhed <abhed@securitybrigade.com>
2023-05-29 22:55:21 +0530

    buy-sell-stock

---
+++ Array/buy-sell-stocks.js
@@ -0,0 +1,55 @@
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    
+    let leftPointer  = 0;
+    let maxProfit    = 0;
+    let rightPointer = leftPointer + 1;
+    
+    while ( rightPointer < prices.length )
+    {
+        if ( prices[leftPointer] < prices[rightPointer] && prices[rightPointer] - prices[leftPointer] > maxProfit )
+        {
+            maxProfit = prices[rightPointer] - prices[leftPointer];
+            rightPointer++;
+            continue;
+        }
+
+        if ( prices[rightPointer] < prices[leftPointer] )
+        {
+            leftPointer = rightPointer;
+            rightPointer++;
+            continue;
+        }
+        
+        rightPointer++;
+    }
+    
+    return maxProfit;
+};
+
+//USING SPACE COMPLEXITY O(N) i.e maxFromRight carry forward technique
+
+    let maxFromRight = [];
+    let max = Number.MIN_SAFE_INTEGER;
+    let maxProfit = 0;
+
+    for ( let i = prices.length - 1; i >= 0; i-- )
+    {
+        if ( prices[i] > max )
+            max = prices[i]
+
+        maxFromRight[i] = max;
+    }
+
+    for ( let i = 0; i < prices.length; i++ )
+    {
+        if ( maxProfit < ( maxFromRight[i] - prices[i] ) )
+            maxProfit = maxFromRight[i] - prices[i];
+    }
+    
+    return maxProfit;
